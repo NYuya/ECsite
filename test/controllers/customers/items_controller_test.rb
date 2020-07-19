7dc978422a5a788b11a2c45dbf58491c183890bd
NYuya <noooochan5@gmail.com>
2020-07-19 10:43:50 +0000

    controller

---
+++ test/controllers/customers/items_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class Customers::ItemsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get customers_items_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get customers_items_show_url
+    assert_response :success
+  end
+
+  test "should get top" do
+    get customers_items_top_url
+    assert_response :success
+  end
+
+end
