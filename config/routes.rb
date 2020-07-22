0a0fdcc815a62628e5b1a202d383b32d60e5302d
NYuya <noooochan5@gmail.com>
2020-07-22 08:34:40 +0000

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,60 +1,25 @@
 Rails.application.routes.draw do
+
   namespace :admins do
-    post 'order_items/update'
+    get '/' => 'admins#top'
+    resources :order_items, only: [:update]
+    resources :orders, only: [:index, :show, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update, :destroy]
+    resources :items
   end
-  namespace :admins do
-    get 'orders/index'
-    get 'orders/show'
-    post 'orders/update'
-  end
-  namespace :admins do
-    get 'customers/index'
-    get 'customers/show'
-    post 'customers/edit'
-    post 'customers/update'
-  end
-  namespace :admins do
-    get 'genres/index'
-    post 'genres/update'
-    post 'genres/create'
-    post 'genres/edit'
-    delete 'genres/destroy'
-  end
-  namespace :admins do
-    get 'items/index'
-    post 'items/update'
-    post 'items/create'
-    post 'items/new'
-    get 'items/show'
-    post 'items/edit'
-    delete 'items/destroy'
-  end
-  namespace :customers do
-    get 'cart_items/index'
-    post 'cart_items/create'
-    delete 'cart_items/destroy'
-    post 'cart_items/update'
-    delete 'cart_items/destroy_all'
-  end
-  namespace :customers do
-    post 'orders/new'
-    post 'orders/create'
-    get 'orders/show'
-    get 'orders/confirm'
-    get 'orders/thanks'
-    get 'orders/index'
-  end
-  namespace :customers do
-    get 'ships/index'
-    post 'ships/edit'
-    delete 'ships/destroy'
-    post 'ships/update'
-    post 'ships/create'
-  end
-  namespace :customers do
-    get 'items/index'
-    get 'items/show'
-    get 'items/top'
-  end
+
+
+  resources :cart_items, only: [:index, :create, :update, :destroy]
+  delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+  resources :orders, only: [:index, :new, :show, :create]
+  post 'orders/confirm' => 'orders#confirm'
+  get 'orders/thanks' => 'orders#thanks'
+  resources :ships, only: [:index, :create, :edit, :update, :destroy]
+  resources :items, only: [:index, :show]
+  root to: 'items#top'
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
