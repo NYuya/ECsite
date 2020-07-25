84eb211079396f15630a7dd0fbb30cc6c3a1c42f
NYuya <noooochan5@gmail.com>
2020-07-25 14:41:24 +0000

    header

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
 
-  devise_for :admins
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
+
+
+
+
   devise_for :customers, controllers: {
     sessions: 'customers/sessions',
     registrations: 'customers/registrations',
