ff9f20ffb7b071c442e1226ba5990ba21f061c52
NYuya <noooochan5@gmail.com>
2020-07-28 16:27:29 +0000

    customers edit

--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -9,12 +9,25 @@ class Customers::CustomersController < ApplicationController
 
   def update
     @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path(current_customer), notice: "successfully updated customer!"
+    else
+      render :edit
+    end
   end
 
   def confirm
   end
 
   def hide
+    current_customer.update(is_member_status: true)
+    reset_session
+    flash[:notice] = "ありがとうございました。またのご利用を心よりお待ちしております。"
+    redirect_to root_path
   end
 
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana,:first_name_kana,:post_code,:address,:phone_number,:is_member_status)
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,19 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+    has_many :orders, dependent: :destroy
+    has_many :cart_items, dependent: :destroy
+    has_many :ships, dependent: :destroy
+
+  def active_for_authentication?
+    super && (self.is_member_status == false)
+  end
+
+  # validates :last_name, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name_kana, presence: true
+  # validates :first_name_kana, presence: true
+  # validates :post_code, presence: true
+  # validates :address, presence: true
+  # validates :phone_number, presence: true
 end
