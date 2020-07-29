244171d92df01f98b0eea974006ad47aa879ccf3
NYuya <noooochan5@gmail.com>
2020-07-29 14:53:28 +0000

    admin item controller

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -1,19 +1,43 @@
 class Admins::ItemsController < ApplicationController
   def index
+    @items = Items.all
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admins_item_path
+    else
+      render :index
+    end
   end
 
   def create
+    item = Item.new(item_params)
+    if item.save
+      redirect_to admins_items_path
+    else
+      @items = Item.all
+      render :index
+    end
   end
 
   def new
+    @item =Item.new
+    @genres = Genre.all
   end
 
   def show
+    @item=Item.find(params[:id])
+    @tax = @item.price.to_i*1.1
   end
 
   def edit
+    @item=Item.find(params[:id])
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name,:description,:image,:price,:is_sale_status,:genre_id)
   end
 end
