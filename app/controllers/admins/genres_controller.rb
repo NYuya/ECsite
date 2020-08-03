750eee3ebc54e6563d6a66fdac7bdf32e9aff817
NYuya <noooochan5@gmail.com>
2020-08-03 07:01:11 +0000

    admin genre new

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,13 +1,31 @@
 class Admins::GenresController < ApplicationController
   def index
+    @genres = Genre.all
+    @newgenre =Genre.new
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admins_genres_path, notice: "successfully created Genre!"
   end
 
   def create
+    @newgenre = Genre.new(genre_params)
+    if @newgenre.save
+      redirect_to admins_genres_path, notice: "successfully created Genre!"
+    else
+      @genres = Genre.all
+      render admins_genres_path
+    end
   end
 
   def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name, :is_active)
   end
 end
