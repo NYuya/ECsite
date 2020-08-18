a41720a4453eed19d83a5bd3f7f989d67facfec8
NYuya <noooochan5@gmail.com>
2020-08-18 12:28:52 +0000

    ship index

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -1,16 +1,41 @@
 class ShipsController < ApplicationController
   def index
+    @ships = Ship.all
+    @ship = Ship.new
   end
 
   def create
+    @ship = Ship.new(ship_params)
+    @ship.customer_id = current_customer.id
+    if @ship.save
+      redirect_to ships_path
+    else
+      @ships = Ship.all
+      render :index
+    end
   end
 
   def edit
+    @ship = Ship.find(params[:id])
   end
 
   def update
+    @ship = Ship.find(params[:id])
+     if @ship.update(ship_params)
+      redirect_to ships_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    ship = Ship.find(params[:id])
+    ship.destroy
+    redirect_to ships_path
+  end
+
+  private
+  def ship_params
+    params.require(:ship).permit(:customer_id, :address, :name, :code)
   end
 end
