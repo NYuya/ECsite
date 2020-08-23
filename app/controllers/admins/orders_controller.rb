6efd1499debf2710b2e449cb93fd9b51e4972203
NYuya <noooochan5@gmail.com>
2020-08-23 13:31:13 +0000

    admin order

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,10 +1,25 @@
 class Admins::OrdersController < ApplicationController
   def index
+    @orders = Order.all
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
+    @freight=800
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to admins_order_path(@order.id)
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:customer_id, :pay_method, :status, :freight, :total, :ship_name, :ship_postcode, :ship_address)
+  end
+  def order_item_params
+    params.require(:order_item).permit(:sat)
   end
 end
